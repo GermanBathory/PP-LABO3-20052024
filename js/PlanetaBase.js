dc73d706aa89ebc9a42d83de122ebc43b5601df5
GermanBathory <germanbathory@gmail.com>
2024-05-20 20:57:48 -0300

    Commit 21pm

---
+++ js/PlanetaBase.js
@@ -0,0 +1,9 @@
+export default class PlanetaBase {
+  constructor(id, nombre, tamaño, masa, tipo) {
+    this.id = id;
+    this.nombre = no;
+    this.tamaño = tamaño;
+    this.masa = masa;
+    this.tipo = tipo;
+  }
+}
